fd2f61928187d2f47cbc7c6bd9e1c70a4fac63a1
juliely8619 <july8619@yahoo.com>
2019-01-03 12:07:09 -0800

    done with prob 1

--- code-challenges/401/Prework-CodeChallenges/Prework401Chall1/Prework401Chall1/Program.cs
+++ code-challenges/401/Prework-CodeChallenges/Prework401Chall1/Prework401Chall1/Program.cs
@@ -11,53 +11,51 @@ namespace Prework401Chall1
         static void Main(string[] args)
         {
             //part 1 of problem 1, collect numbers from user and populate array
-            int guessCounter = 0;
-            string[] numArray = new string[5];
-            string tempNum = "";
+            int p1GuessCounter = 0;
+            string[] p1NumArray = new string[5];
+            string p1TempNum = "";
             do
             {
                 Console.WriteLine("Please enter a number between 1 to 10?");
-                tempNum = Console.ReadLine();
-                if (tempNum == "1" || tempNum == "2" || tempNum == "3" || tempNum == "4" || tempNum == "5" || tempNum == "6" || tempNum == "7" || tempNum == "8" || tempNum == "9" || tempNum == "10")
+                p1TempNum = Console.ReadLine();
+                if (p1TempNum == "1" || p1TempNum == "2" || p1TempNum == "3" || p1TempNum == "4" || p1TempNum == "5" || p1TempNum == "6" || p1TempNum == "7" || p1TempNum == "8" || p1TempNum == "9" || p1TempNum == "10")
                 {
-                    numArray[guessCounter] = tempNum;
-                    //Console.WriteLine(numArray[guessCounter]); //this shows there is info in the array
-                    guessCounter++;
+                    p1NumArray[p1GuessCounter] = p1TempNum;
+                    p1GuessCounter++;
                 }
                 else
                 {
                     Console.WriteLine("that isn't a valid entry");
                 }
-            } while (guessCounter < 5);
+            } while (p1GuessCounter < 5);
 
             //part 2 of problem 1: prints array and ask user for a number and return a score
             Console.Write("The numbers in the list: ");
             for (int i = 0; i < 5;i++)
             {
-                Console.Write(numArray[i] + " ");
+                Console.Write(p1NumArray[i] + " ");
             }
             Console.WriteLine("Please select a number from the list?");
-            string userChoice = Console.ReadLine();
-            Console.WriteLine("userschoice " + userChoice); //just for testing
-            //it defaults to 50, and if I put in a number it does i dont know math to it (1 put it at 49, 3 put it at 51
-            ArrayMax(numArray,userChoice);
+            int userChoice = Convert.ToInt32(Console.ReadLine());
+            //******calls prob 1 method (just highlighting for easier grading) *****//
+            ArrayMax(p1NumArray, userChoice);
+            //*****end of prob 1 in MAIN ****//
             Console.ReadLine();
         }
 
-        private static void ArrayMax(string[] array, string selection)
+        private static void ArrayMax(string[] array, int selection)
         {
-            int scoreMult1 = 1;
-            int scoreMult2 = 1;
-            int scoreMult3 = 1;
-            int scoreMult4 = 1;
-            int scoreMult5 = 1;
-            int scoreMult6 = 1;
-            int scoreMult7 = 1;
-            int scoreMult8 = 1;
-            int scoreMult9 = 1;
-            int scoreMult10 = 1;
-            int score = 1;
-            int intSelection = 0;
+            int scoreMult1 = 0;
+            int scoreMult2 = 0;
+            int scoreMult3 = 0;
+            int scoreMult4 = 0;
+            int scoreMult5 = 0;
+            int scoreMult6 = 0;
+            int scoreMult7 = 0;
+            int scoreMult8 = 0;
+            int scoreMult9 = 0;
+            int scoreMult10 = 0;
+            int score = 0;
 
             //iterate through array and count number of times a number was selected
             for (int i = 0; i < 5; i++)
@@ -83,49 +81,31 @@ namespace Prework401Chall1
                 else if (array[i] == "10")
                     scoreMult10++;
             }
-            Console.WriteLine("selection :" + selection); //just for testing
-            if (selection == "1")
-            {
-                intSelection = 1;
-                score = intSelection * scoreMult1;
-            }
-            else if (selection == "2")
-            {
-                intSelection = 2;
-                score = intSelection * scoreMult2;
-            }
-            else if (selection == "3")
-            {
-                intSelection = 3;
-                score = intSelection * scoreMult3;
-            }
-            else if (selection == "4")
-            {
-                intSelection = 4;
-                score = intSelection * scoreMult4;
-            }
-            else if (selection == "5")
-            {
-                intSelection
-                score = intSelection * scoreMult5;
-            }
-            else if (selection == "6")
-            {
-                intSelection
-                score = intSelection * scoreMult6;
-            }
-            else if (selection == "7")
-                score = intSelection * scoreMult7;
-            else if (selection == "8")
-                score = intSelection * scoreMult8;
-            else if (selection == "9")
-                score = intSelection * scoreMult9;
-            else if (selection == "10")
-                score = intSelection * scoreMult10;
+            if (selection == 1)
+                score = selection * scoreMult1;
+            else if (selection == 2)
+                score = selection * scoreMult2;
+            else if (selection == 3)
+                score = selection * scoreMult3;
+            else if (selection == 4)
+                score = selection * scoreMult4;
+            else if (selection == 5)
+                score = selection * scoreMult5;
+            else if (selection == 6)
+                score = selection * scoreMult6;
+            else if (selection == 7)
+                score = selection * scoreMult7;
+            else if (selection == 8)
+                score = selection * scoreMult8;
+            else if (selection == 9)
+                score = selection * scoreMult9;
+            else if (selection == 10)
+                score = selection * scoreMult10;
+            else
+                Console.Write("You didn't pick a possible number in the list");
  
             Console.Write("your score is: ");
-            Console.WriteLine(score); //score is zero because that was my defaul and I never make it into my if's
-            Console.ReadLine();
+            Console.WriteLine(score); 
         }
     }
 }
@@ -134,4 +114,13 @@ namespace Prework401Chall1
 Questions: 
 -How do I add documents? (like a readme.md)
 -from my research, i can't just show the array, i have to loop through it, can you verify this is true
+-when submit note this was in .netframework and already talked to amanada and i wouldn't lose points for it.
+
+    
+-note to self use .net core moving forward
+-note to self, can use ubuntu for git ACP still if want but should get famil with power shell (not same as CDM)
+-be able to and famil with ACP in VS (as MS uses that)
+-but it really is whatever is most comfortable for you
+-add docs with right click on folder and add file
+
 */
